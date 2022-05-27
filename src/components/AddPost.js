4b582501f42d2e23d88d22413e3b4dbe4529c54f
hellohyonee <hellohk07@gmail.com>
2022-05-27 18:47:09 +0900

    컴포넌트 구성. post 추가, post 목록(+post 세부내용)을 HomePage컴포넌트에서 출력

---
+++ src/components/AddPost.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AddPost() {
+  return (
+    <div>
+      <h1>Add Post</h1>
+    </div>
+  );
+}
---
+++ src/components/PostList.js
@@ -0,0 +1,11 @@
+import React from "react";
+import PostDetail from "./PostDetail";
+
+export default function PostList() {
+  return (
+    <div>
+      <h1>Post List</h1>
+      <PostDetail />
+    </div>
+  );
+}
