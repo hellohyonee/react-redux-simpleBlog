1d369886966383f9bc477b33c3d825dadb4f5154
hellohyonee <hellohk07@gmail.com>
2022-05-30 15:22:43 +0900

    PostDetail - card 형식으로 post 구성, 삭제 버튼 추가

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -2,8 +2,19 @@ import React from "react";
 
 export default function PostDetail() {
   return (
-    <div>
-      <h1>Post Detail</h1>
+    <div className="col s12 mg">
+      <div className="card">
+        <div className="card-content">
+          <span className="card-title">Post one</span>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+            libero quidem molestiae!
+          </p>
+        </div>
+        <div className="card-action">
+          <button className="btn red">Delete</button>
+        </div>
+      </div>
     </div>
   );
 }
