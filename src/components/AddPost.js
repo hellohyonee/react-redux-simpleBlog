26a00303151e76aa9a644ae7af9a9e2e896e629f
hellohyonee <hellohk07@gmail.com>
2022-05-30 15:10:57 +0900

    AddPost에 input요소와 button 요소 추가

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -2,8 +2,28 @@ import React from "react";
 
 export default function AddPost() {
   return (
-    <div>
-      <h1>Add Post</h1>
+    <div className="container">
+      <h5 className="blue-text center-align">HyoneeBlog</h5>
+      <form>
+        <div className="input-field">
+          <label className="active" htmlFor="post_title">
+            Title
+          </label>
+          <input type="text" name="title" />
+        </div>
+        <div className="input-field">
+          <label className="active" htmlFor="post_content">
+            Content
+          </label>
+          <textarea name="content" className="materialize-textarea"></textarea>
+        </div>
+        <div className="input-field">
+          <button className="btn blue">
+            Submit
+            <i className="material-icons right">send</i>
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
