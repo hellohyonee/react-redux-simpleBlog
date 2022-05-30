5443edc125fbac47191c2397b0e981a08b4afbbc
hellohyonee <hellohk07@gmail.com>
2022-05-30 18:00:44 +0900

    mapDispatchToProps()함수로 dispatch하여 action을 전달하고 상태를 변화

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -11,11 +11,22 @@ function PostDetail(props) {
           <p>{post.content}</p>
         </div>
         <div className="card-action">
-          <button className="btn red">Delete</button>
+          <button onClick={() => props.deletePost(post.id)} className="btn red">
+            Delete
+          </button>
         </div>
       </div>
     </div>
   );
 }
 
-export default connect()(PostDetail);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    deletePost: (id) => {
+      dispatch({ type: "DELETE_POST", id });
+    },
+  };
+};
+
+// connect()함수 첫번째 파라미터에 mapStateToProps가 없으므로 null로 처리
+export default connect(null, mapDispatchToProps)(PostDetail);
