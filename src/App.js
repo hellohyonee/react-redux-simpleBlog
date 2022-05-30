2d5986432abc16af7ea6abac54620a401ed3de94
hellohyonee <hellohk07@gmail.com>
2022-05-30 15:09:59 +0900

    materialize css/icons 적용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import "materialize-css/dist/css/materialize.css";
+import "material-design-icons/iconfont/material-icons.css";
 import { HomePage } from "./components/HomePage";
 
 function App() {
