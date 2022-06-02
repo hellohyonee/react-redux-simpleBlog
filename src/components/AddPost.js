617f6244a7a411ee7025103018bd0e588465ab0c
hellohyonee <hellohk07@gmail.com>
2022-06-02 17:57:20 +0900

    handleChange(), handleSubmit() 구현

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function AddPost() {
+  const [post, setPost] = useState({ post: { id: 3, title: "", content: "" } });
+
+  const handelChange = (e) => {
+    setPost({ post: { ...post.post, [e.target.name]: e.target.value } });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(post.post);
+    setPost({ post: { title: "", content: "" } });
+  };
+
   return (
     <div className="container" style={{ margin: "4rem auto" }}>
       <h5 className="blue-text center-align">HyoneeBlog</h5>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="input-field">
           <label className="active" htmlFor="post_title">
             Title
           </label>
-          <input type="text" name="title" />
+          <input
+            onChange={handelChange}
+            value={post.title}
+            type="text"
+            name="title"
+          />
         </div>
         <div className="input-field">
           <label className="active" htmlFor="post_content">
             Content
           </label>
-          <textarea name="content" className="materialize-textarea"></textarea>
+          <textarea
+            onChange={handelChange}
+            value={post.content}
+            name="content"
+            className="materialize-textarea"
+          ></textarea>
         </div>
         <div className="input-field">
           <button className="btn blue">
