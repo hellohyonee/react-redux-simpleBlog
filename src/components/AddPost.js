9b32fc99d468071f113e9623fbe2c1ebe3f90ca5
hellohyonee <hellohk07@gmail.com>
2022-07-11 19:41:22 +0900

    title과 content 내용이 없을 경우, alert으로 내용 채우라고 알리기

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { v4 as uuidv4 } from "uuid";
-import { addPost } from "../store/actions/postAction";
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addPost } from '../store/actions/postAction';
 
 export default function AddPost() {
   const dispatch = useDispatch();
   const [post, setPost] = useState({
-    id: "",
-    title: "",
-    content: "",
+    id: '',
+    title: '',
+    content: '',
   });
 
   const handelChange = (e) => {
@@ -19,29 +19,28 @@ export default function AddPost() {
   };
 
   const handleSubmit = (e) => {
+    if ((post.title === '') | (post.content === '')) {
+      alert('내용을 채워주세요.');
+      return null;
+    }
     e.preventDefault();
     dispatch(addPost(post));
     setPost({
       id: uuidv4(),
-      title: "",
-      content: "",
+      title: '',
+      content: '',
     });
   };
 
   return (
-    <div className="container" style={{ margin: "4rem auto" }}>
+    <div className="container" style={{ margin: '4rem auto' }}>
       <h5 className="blue-text center-align">HyoneeBlog</h5>
       <form onSubmit={handleSubmit}>
         <div className="input-field">
           <label className="active" htmlFor="post_title">
             Title
           </label>
-          <input
-            onChange={handelChange}
-            value={post.title}
-            type="text"
-            name="title"
-          />
+          <input onChange={handelChange} value={post.title} type="text" name="title" />
         </div>
         <div className="input-field">
           <label className="active" htmlFor="post_content">
